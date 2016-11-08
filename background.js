c1c4d1a63ea161dde61e166eaf5f14ccd38d360f
xpl <rocket.mind@gmail.com>
2016-11-09 02:31:36 +0300

    now uses clever hack to update menu synchronously from content script

--- background.js
+++ background.js
@@ -1,61 +1,36 @@
 let menu = null
-
-const updateMenu = (text, link) => {
-    if (!text) {
-        if (menu) {
-            chrome.contextMenus.remove (menu.id)
-            menu = null
-        }
-    } else {
-        if (!menu) {
-            chrome.contextMenus.create (menu = {
-                'id': 'mutko', 
-                'enabled': true, 
-                'title': text,
-                "contexts": ["selection"],
-                onclick: () => {
-                     chrome.tabs.create ({ url: menu.link })
-                }
-            })
-        } else {
-            chrome.contextMenus.update (menu.id, { title: text })
-        }
-
-        menu.link = link
-    }
-}
+let link = null
 
 chrome.runtime.onMessage.addListener (msg => {
 
     if (msg.request === 'updateContextMenu') {
 
-        if (msg.selection && (msg.selection.split (/\s+/).length < 3)) {
+        link = msg.link
 
-            updateMenu ('Перевод...')
+        if (!msg.text) {
 
-            fetch (new Request ('https://api.lingvolive.com/Translation/WordListPart?prefix=' + msg.selection + '&srcLang=1033&dstLang=1049&pageSize=10&startIndex=0'))
-                .then (r => r.json ())
-                .then (json => {
-
-                    if (json.items.length && json.items[0].lingvoTranslations) {
-
-                        updateMenu (json.items[0].lingvoTranslations, 'https://www.lingvolive.com/ru-ru/translate/en-ru/' + msg.selection)
-                    } else {
-
-                        updateMenu ()
-                    }
-
-                })
-                .catch (e => {
-                    updateMenu (e)
-                })
+            if (menu) {
+                chrome.contextMenus.remove (menu.id)
+                menu = null
+            }
 
         } else {
-            updateMenu ()
-        }
 
-        // setTimeout (() => {
-        //     chrome.contextMenus.update (menu.id, { title: msg.selection })
-        // }, 2000)
+            if (!menu) {
+
+                chrome.contextMenus.create (menu = {
+                    id: 'mutko', 
+                    enabled: link ? true : false, 
+                    title: msg.text,
+                    contexts: ['selection'],
+                    onclick: () => {
+                         chrome.tabs.create ({ url: link })
+                    }
+                })
+
+            } else {
+                chrome.contextMenus.update (menu.id, { title: msg.text, enabled: link ? true : false })
+            }
+        }
     }
-});
+})
--- mutko.js
+++ mutko.js
@@ -1,10 +1,47 @@
-document.addEventListener ('selectionchange', () => {
+const fetchSync = url => {
 
-    const selection = window.getSelection().toString().trim()
+    const xhr = new XMLHttpRequest ()
 
-    chrome.runtime.sendMessage ({
-        request: 'updateContextMenu',
-        selection: selection
-    })
+    xhr.open ('GET', url, false /* SYNCHRONOUS XHR FTW :) */)
+    xhr.send (null)
+
+    return xhr.responseText
+}
+
+const updateMenu = data => {
+	chrome.runtime.sendMessage (Object.assign ({ request: 'updateContextMenu' }, data))
+}
+
+document.addEventListener ('contextmenu', e => {
+
+	const sel = window.getSelection ().toString ().trim ()
+
+	const url = 'https://api.lingvolive.com/Translation/WordListPart?prefix=' + sel + '&srcLang=1033&dstLang=1049&pageSize=10&startIndex=0'
+	
+	if (sel && (sel.split (/\s+/).length < 3)) {
+
+        try {
+
+            const json = JSON.parse (fetchSync (url))
+
+            const translations = json.items.map (item => item.lingvoTranslations)
+
+            if (translations.length) {
+                updateMenu ({
+                	text: translations[0],
+                	link: 'https://www.lingvolive.com/ru-ru/translate/en-ru/' + sel })
+            } else {
+                updateMenu ()
+            }
+
+        } catch (e) {
+            updateMenu ({ text: String (e) })
+        }
+
+    } else {
+    	updateMenu ()
+    }
+
+    fetchSync (url) // HACK: delays context menu, so the 'updateContextMenu' message gets processed first
 })
 
